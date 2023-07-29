c7ac09de5b828baaa0ed9f69541a4ac9b2417caf
Md Ahmed Khan <alifftw@gmail.com>
2023-07-29 17:33:26 -0400

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [data, setData] = useState("Loading...");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setData(data.message);
+        console.log(data); // Log the updated data here
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle error states here if necessary
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>{data}</h1>
+    </div>
+  );
+}
+
+export default Home;
